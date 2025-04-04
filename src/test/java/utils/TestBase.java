33676766d25472f9ef35a35ae79615cbf2252255
Karthik Lingasamy (NCS) <Karthiklingasamy@gmail.com>
2025-04-04 15:00:27 +0800

    My  Commite up to lession 38 include how to read value from global property file and read chrome driver

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -3,16 +3,34 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestBase {
 
     public WebDriver driver;
 
-    public WebDriver WebDriverManager(){
+    public WebDriver WebDriverManager() throws IOException {
+
+        FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"//src//test//resources//global.properites");
+        Properties prop = new Properties();
+        prop.load(fis);
+        String url=prop.getProperty("QAurl");
 
          if(driver==null){
-             System.setProperty("webdriver.chrome.driver","C:/QAF/chromedriver-win64/chromedriver.exe");
-             driver = new ChromeDriver();
-             driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+             if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+                 System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//src//test//resources//chromedriver.exe");
+                 driver = new ChromeDriver();
+             }
+
+             if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+                 //System.setProperty("webdriver.chrome.driver", "C:/QAF/chromedriver-win64/chromedriver.exe");
+                 //driver = new ChromeDriver();
+             }
+
+             driver.get(url);
          }
          return driver;
 
--- src/test/java/utils/TextContextSetup.java
+++ src/test/java/utils/TextContextSetup.java
@@ -3,6 +3,8 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import pageObjects.PageObjectManager;
 
+import java.io.IOException;
+
 public class TextContextSetup {
 
     public WebDriver driver;
@@ -11,7 +13,7 @@ public class TextContextSetup {
     public TestBase testBase;
     public GenericUtils genericUtils;
 
-    public TextContextSetup(){
+    public TextContextSetup() throws IOException {
         testBase=new TestBase();
         pageObjectManager = new PageObjectManager(testBase.WebDriverManager());
         genericUtils=new GenericUtils(testBase.WebDriverManager());
